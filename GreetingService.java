e76c696e16e07cbc1e3eed21abfdc553cdd5d1c9
Анна <anna_bo@list.ru>
2015-10-25 14:49:38 +0200

    first commit

---
+++ GreetingService.java
@@ -0,0 +1,12 @@
+public class GreetingService {
+
+	public static void main(String[] args) {
+		
+	    if (args.length > 0) {
+			  System.out.println("Hello " + args[0]);
+	    }
+	    else { 
+	        System.out.println("Print 'java GreetingService' + name ");
+	    	}
+	}
+}
